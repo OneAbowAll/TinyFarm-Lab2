9c3c21844f3b3e68de026ee5671a4044f363b0b7
OneAbowAll <dado.delgaudio@gmail.com>
2022-05-26 16:23:01 +0200

    Gestion input opzionale con getopt + Aggiunta di xerrori.c + Aggiunta di .gitignore (non so se funzionerà)

--- farm.c
+++ farm.c
@@ -1,11 +1,49 @@
+//Master Worker
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int getopt(int argc, char * const argv[],
+          const char *optstring);
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
+  int opt;
+  int nthread = 4, qlen = 8, delay = 0;
+
   // controlla numero argomenti
   if(argc<2) {
       printf("Uso: %s file [file ...] \n",argv[0]);
-      return 1;
+      //return 1;
   }
 	
+  //Prendi argomenti opzionali
+  while((opt = getopt(argc, argv, "n:q:t:")) != -1){
+    switch (opt)
+    {
+    case 'n':
+      nthread =  atoi(optarg);
+      break;
+     
+    case 'q':
+      qlen =  atoi(optarg);
+      break;
+    
+    case 't':
+      delay =  atoi(optarg);
+      break;
+
+    default: /* Caso ? */
+      fprintf(stdout, "Parametro opzionale errato. Uso: %s file [file...] [-n nthread] [-q qlen] [-t delay].\n", argv[0]);
+      break;
+    }
+  }
+
+  fprintf(stdout, "Parametri attuali => nthread: %d; qlen: %d; delay = %d \n", nthread, qlen, delay);
 	return 0;
 }
---
+++ makefile
@@ -0,0 +1,19 @@
+# definizione del compilatore e dei flag di compilazione
+# che vengono usate dalle regole implicite
+CC=gcc
+CFLAGS=-g -Wall -O -std=c99
+LDLIBS=-lm -lrt -pthread
+
+# eseguibili da costruire
+EXECS=farm
+
+.PHONY: clean
+
+all: $(EXECS)
+
+# dipendenze
+farm: farm.o xerrori.o
+
+# target che cancella eseguibili e file oggetto
+clean:
+	rm -f $(EXECS) *.o  *.zip
